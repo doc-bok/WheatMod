b685b85935647f6ba5b206feefdfdadac6ad71a4
doc-bok <41854155+doc-bok@users.noreply.github.com>
2020-01-05 16:11:45 +0800

    Fixed "specialization" of spawn check

--- src/main/java/com/bokmcdok/wheat/entity/feldgeister/getreidewolf/ModGetreidewolfEntity.java
+++ src/main/java/com/bokmcdok/wheat/entity/feldgeister/getreidewolf/ModGetreidewolfEntity.java
@@ -263,9 +263,9 @@ public class ModGetreidewolfEntity extends ModFeldgeisterEntity implements IRang
      * @param reason The spawn reason.
      * @param position The block's position.
      * @param random The random number generator.
-     * @return TRUE if a widowbird can spawn here.
+     * @return TRUE if the entity can spawn here.
      */
-    public static boolean canSpawn(EntityType<ModGetreidewolfEntity> entity, IWorld world, SpawnReason reason, BlockPos position, Random random) {
+    public static boolean canGetreideWolfSpawn(EntityType<ModGetreidewolfEntity> entity, IWorld world, SpawnReason reason, BlockPos position, Random random) {
         return world.getCurrentMoonPhaseFactor() > 0.9f && ModFeldgeisterEntity.canSpawn(entity, world, reason, position, random);
     }
 
