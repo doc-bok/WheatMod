81377e87fd4d7a70fbfc72fc7d49fe7ab67605fe
doc-bok <41854155+doc-bok@users.noreply.github.com>
2020-01-15 07:42:11 +0800

    Removed unused import

--- src/main/java/com/bokmcdok/wheat/item/ModItemImpl.java
+++ src/main/java/com/bokmcdok/wheat/item/ModItemImpl.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
