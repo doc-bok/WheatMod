8af4a87db8e9925fe7237de7a319818056de71dc
doc-bok <41854155+doc-bok@users.noreply.github.com>
2020-02-10 15:35:07 +0800

    Updated dimension code so it doesn't litter the global namespace.

--- src/main/java/com/bokmcdok/wheat/WheatMod.java
+++ src/main/java/com/bokmcdok/wheat/WheatMod.java
@@ -1,6 +1,7 @@
 package com.bokmcdok.wheat;
 
 import com.bokmcdok.wheat.container.ModContainerUtils;
+import com.bokmcdok.wheat.dimension.ModDimensionRegistrar;
 import com.bokmcdok.wheat.screen.FlourMillScreen;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraftforge.fml.common.Mod;
@@ -16,9 +17,15 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class WheatMod
 {
     public static final String MOD_ID = "docwheat";
+    private final ModForgeEventHandler mForgeEventHandler;
+    private final ModDimensionRegistrar mDimensionRegistrar;
 
     public WheatMod() {
+        mForgeEventHandler = new ModForgeEventHandler();
+        mDimensionRegistrar = new ModDimensionRegistrar();
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+
+
     }
 
     private void clientSetup(final FMLClientSetupEvent event) {
--- src/main/java/com/bokmcdok/wheat/dimension/ModDimensionRegistrar.java
+++ src/main/java/com/bokmcdok/wheat/dimension/ModDimensionRegistrar.java
@@ -1,25 +1,28 @@
 package com.bokmcdok.wheat.dimension;
 
-import com.bokmcdok.wheat.WheatMod;
 import com.bokmcdok.wheat.dimension.wheat.ModWheatDimensionFactory;
 import net.minecraftforge.common.ModDimension;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ObjectHolder;
 
-@Mod.EventBusSubscriber(modid = WheatMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModDimensionRegistrar {
 
     @ObjectHolder("docwheat:wheat_dimension")
     public static final ModDimension WHEAT_DIMENSION = null;
 
     /**
+     * Construction
+     */
+    public ModDimensionRegistrar() {
+        FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(ModDimension.class, this::onDimensionRegistryEvent);
+    }
+
+    /**
      * Register new dimensions.
      * @param event The registry event.
      */
-    @SubscribeEvent
-    public static void onDimensionRegistryEvent(RegistryEvent.Register<ModDimension> event) {
+    private void onDimensionRegistryEvent(RegistryEvent.Register<ModDimension> event) {
         event.getRegistry().register(new ModWheatDimensionFactory().setRegistryName("docwheat:wheat_dimension"));
     }
 }
